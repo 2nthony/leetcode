1ffe7ab9db0be4cd139353cd3bbc8f390ab858cf
evillt <hujiehao@hongrongtech.com>
2018-05-22 18:31:22 +0800

    其他的算法题目

---
+++ others/solutions/1.js
@@ -0,0 +1,11 @@
+const single = (arr, t) => {
+  let index = []
+  for (const i in arr) {
+    if (a.indexOf(t - arr[i] > -1)) {
+      index.push(parseInt[i])
+    }
+  }
+  return index
+}
+
+single([3, 8, 10, 16], 19)
